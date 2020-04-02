75a3808df960b62278fd0b6625f7e0e18d8b61d0
Chris Cheung <chris.cheung@welab.co>
2020-04-02 11:12:21 +0800

    change to let FactItem handle promise waiting logic, implement top5, and use localStorage as cache

--- js/app.js
+++ js/app.js
@@ -12,13 +12,22 @@ const NumberInput = (props) => {
     </form>
 }
 
-const FactItem = ({text}) => {
+const FactItem = ({factPromise}) => {
+    let [text, setText] = React.useState("fetching facts for " + factPromise.num + "...")
+    factPromise.then(setText)
+
     return <li>{text}</li>
 }
 
-const FactList = (props) => {
+const FactList = ({facts}) => {
+    let top5 = []
+    for (let index = 0; index < facts.length && index < 5; index++) {
+        const element = facts[index];
+        top5.push(element)
+    }
+
     return <ul>{
-        props.facts.map(f => <FactItem text={f}/> )
+        top5.map(f => <FactItem key={f.key} factPromise={f}/> )
     }</ul>
 }
 
@@ -26,12 +35,25 @@ const App = (props) => {
     const [facts, setFacts] = React.useState([])
 
     const handleSubmit = text => {
-        let promise = fetchFact(text)
+        let key = "fact_" + text
+        let cache = localStorage.getItem(key)
+        let promise = null
+        if (cache) {
+            promise = Promise.resolve(cache)
+        } else {
+            promise = fetchFact(text)
+            promise.then(fact => {
+                localStorage.setItem(key, fact)
+            })
+        }
+        promise.num = text
+        promise.key = facts.length
+        setFacts([promise, ...facts])
     }
 
     return <div>
         <h1>Number Facts</h1>
-        <NumberInput onSubmit={text => setFacts([text, ...facts])} />
+        <NumberInput onSubmit={handleSubmit} />
         <FactList facts={facts}/>
     </div>
 }
